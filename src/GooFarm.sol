c92b2cda50b77520ebded5d81d905fe0205d718a
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-09-19 05:12:00 +0200

    Started implementing GooFarm

--- src/GooFarm.sol
+++ src/GooFarm.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-contract GooFarm {}
+import {ERC4626} from "solmate/mixins/ERC4626.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract GooFarm is ERC4626 {
+    constructor(ERC20 goo) ERC4626(goo, "Farmed Goo", "xGOO") {}
+
+    // Returns total GOO (less Gobbler and protocol fees) in the protocol
+    function totalAssets() public view override returns (uint256) {
+        // TODO
+        return 1;
+    }
+}
